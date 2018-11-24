ee28999da378f3142601e6f644038244bd2efedb
Unknown <daniel.tesanovic97@gmail.com>
2018-11-24 19:01:10 +0000

    Working models v1
    ples work 3q

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Hobby)
+admin.site.register(Member)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    image = models.ImageField(upload_to="profile_images")
+    dob = models.DateField(auto_now=False, auto_now_add=False)
+
+
+class Member(User):
+    profile = models.OneToOneField(
+        to=Profile,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+    )
+    hobbies = models.ManyToManyField(
+        to='Hobby',
+        blank=True,
+        symmetrical=False,
+        related_name='related_to'
+    )
+    likes = models.ManyToManyField(
+        to='self'   ,
+        blank=True,
+        symmetrical=True
+    )
+    #FFFFFFFFFFFFUUUUUUUUUUUUUUUCCCCCCCCCCCCCKKKKKKKKKKKKKDDDDDDDDDIIIIIIIIIISSSSSSSSSSSSSS
+
+class Hobby(models.Model):
+    name = models.CharField(max_length=254)
+    desc = models.CharField(max_length=254)
